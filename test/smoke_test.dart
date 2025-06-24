c6429c4ef33d9ff60b8c424bfe5dd03432cf3fef
Sandeep <krishnasandeep.g@gmail.com>
2025-06-24 18:44:54 +0100

    Add initial smoke test to verify basic arithmetic functionality

---
+++ test/smoke_test.dart
@@ -0,0 +1,8 @@
+// filepath: /Users/sandy/Dev/lonepeak/lonepeak/test/smoke_test.dart
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('smoke test', () {
+    expect(1 + 1, 2);
+  });
+}
